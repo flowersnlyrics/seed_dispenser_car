5dd83622353bc0864b4210a5ab685b2a932617fb
Jennifer de Souza <jenniferdesouza88@gmail.com>
2019-04-17 00:09:27 -0400

    Changed pinouts. For some reason PA6 is not working, changed WHEEL_MOTOR_IN3 to PA0 instead

--- Src/main.c
+++ Src/main.c
@@ -42,7 +42,7 @@ int main(void)
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   MX_TIM2_Init();
-  MX_USART2_UART_Init();
+  //MX_USART2_UART_Init();
   MX_I2C1_Init();
   //accel_if_init(); 
   mag_ctrl_init(); 
